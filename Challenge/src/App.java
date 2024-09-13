38c0915738a13add12bfa82352e43956b8ae4ddf
markko17 <marklagdaan606@gmail.com>
2024-09-13 10:34:19 +0800

    first commit

---
+++ Challenge/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+        String name = "makoy";
+        int age = 19;
+        double GPA = 90.5; 
+        char bloodtype = 'O';
+
+        System.out.println("my name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My GPA is " + GPA);
+        System.out.println("My blood type is " + bloodtype);
+    }
+}
---
+++ Challenge/src/challenge2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class challenge2 {
+    public static void main(String[] args) {
+
+        int x;
+        int y;
+        int result;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Num1: ");
+        x = scan.nextInt();
+
+        System.out.print("Num2: ");
+        y = scan.nextInt();
+
+        result = x + y;
+
+        System.out.print(x + " + " + y + " = ");
+        System.out.println(result);
+        
+
+        
+         
+
+
+
+        
+    }
+
+    private static int x(int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'x'");
+    }
+
+}
